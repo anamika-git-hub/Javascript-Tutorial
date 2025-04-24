afdebe18bf1a128a242052ef5c4d55fb9f859481
anamika-git-hub <anamikap6840@gmail.com>
2025-04-24 10:54:19 +0530

    fix: typing error on iteration methods corrected

--- ArrayMethods/IterationMethods.js
+++ ArrayMethods/IterationMethods.js
@@ -5,20 +5,20 @@ let array = ['a','b','c']
 //---------------ENTRIES
 
 let entry = array.entries();
-for(let entry of a){
-    console.log(entry); // output: [ 0, 'a' ] [ 1, 'b' ] [ 2, 'c' ]
+for(let a of entry){
+    console.log(a); // output: [ 0, 'a' ] [ 1, 'b' ] [ 2, 'c' ]
 }
 
 //-------------KEYS
 
 let key = array.keys();
-for(let entry of a){
-    console.log(entry);// output: 0 1 2
+for(let a of key){
+    console.log(a);// output: 0 1 2
 }
 
 //-------------VALUES
 
 let value = array.values();
-for(let entry of a){
-    console.log(entry);// output: a b c
+for(let a of value){
+    console.log(a);// output: a b c
 }
